3e8e2d0297a3465a359ed59161180148efcf3112
mailtosangeethadevidi-collab <mailtosangeethadevidi@gmail.com>
2026-01-16 08:26:43 +0530

    Time: 4 ms (42.85%) | Memory: 43.2 MB (62.24%) - LeetSync

---
+++ 20-valid-parentheses/valid-parentheses.java
@@ -0,0 +1,49 @@
+import java.util.Stack;
+class Solution {
+    public boolean isValid(String s) {
+     Stack<Character> st = new Stack<>();
+     for(int i=0;i<s.length();i++){
+        char ch =s.charAt(i);
+        if(ch=='(' ||  ch=='{' || ch=='['){
+            st.push(ch);
+        }
+        else if(ch==']'){
+            if(!st.isEmpty()){
+            if( st.peek()=='['){
+                st.pop();
+            } 
+            else
+              return false;
+           }
+            else
+              return false;
+        }
+        else if(ch=='}'){
+            if(!st.isEmpty()){
+            if(st.peek()=='{'){
+                st.pop();
+            }
+            else 
+             return false;
+            }
+            else 
+             return false;
+        }
+        else if(ch==')'){
+            if(!st.isEmpty()){
+            if(st.peek()=='('){
+                st.pop();
+            }
+            else 
+             return false;
+            }
+            else 
+              return false;
+        }
+     }
+     if(st.isEmpty()){
+        return true;
+     }
+     return false;
+    }
+}
