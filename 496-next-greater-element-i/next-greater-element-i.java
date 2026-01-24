63318c3107956fafbe4ba399fa3a9e1151d34f53
mailtosangeethadevidi-collab <mailtosangeethadevidi@gmail.com>
2026-01-24 10:00:25 +0530

    Time: 3 ms (92.33%) | Memory: 44.8 MB (87.72%) - LeetSync

--- 496-next-greater-element-i/next-greater-element-i.java
+++ 496-next-greater-element-i/next-greater-element-i.java
@@ -1,33 +1,25 @@
 class Solution {
     public int[] nextGreaterElement(int[] nums1, int[] nums2) {
         int ans[]=new int[nums1.length];
-        int n=-1;
-        int index=0;
-        for(int i=0;i<nums1.length;i++){
-            int flag=0;
-            int j=0;
-            for(j=0;j<nums2.length;j++){
-                if(nums1[i]==nums2[j]){
-                    flag=1;
-                    break;
-                }
+       for(int i=0;i<nums1.length;i++){
+        int j=0;
+        for(j=0;j<nums2.length;j++){
+            if(nums1[i]==nums2[j]){
+                break;
             }
-            int f=0;
-            for(int k=j;k<nums2.length;k++){
-                        if(nums2[k]>nums2[j]){
-                            n=nums2[k];
-                            f=1;
-                            break;
-                        }
+        }
+        int flag=0;
+        for(int k=j;k<nums2.length;k++){
+            if(nums2[k]>nums2[j]){
+                ans[i]=nums2[k];
+                flag=1;
+                break;
             }
-            if(f==1){
-                ans[index++]=n;
-            }
-            else{
-                ans[index++]=-1;
-            }
-
-            }
-            return ans;
+        }
+        if(flag==0){
+            ans[i]=-1;
+        }
+    }
+     return ans;       
     }
 }
