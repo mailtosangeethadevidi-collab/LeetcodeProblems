fcd4ed6a9989e41a136950bcd3ce30cb3e3d4ac5
mailtosangeethadevidi-collab <mailtosangeethadevidi@gmail.com>
2026-01-30 20:51:35 +0530

    Time: 6 ms (48.50%) | Memory: 47.3 MB (12.02%) - LeetSync

--- 557-reverse-words-in-a-string-iii/reverse-words-in-a-string-iii.java
+++ 557-reverse-words-in-a-string-iii/reverse-words-in-a-string-iii.java
@@ -4,20 +4,12 @@ class Solution {
         StringBuilder sb = new StringBuilder();
         for(int i=0;i<arr.length;i++){
            StringBuilder sb1 = new StringBuilder(arr[i]);
-            int j=0;
-            int k=sb1.length()-1;
-            while(j<k){
-                char temp=sb1.charAt(j);
-                sb1.setCharAt(j,sb1.charAt(k));
-                sb1.setCharAt(k,temp);
-                j++;
-                k--;
-            }
+            
            if(i!=arr.length-1){
-            sb.append(sb1.toString()+" ");
+            sb.append(sb1.reverse()+" ");
            }
            else{
-            sb.append(sb1.toString());
+            sb.append(sb1.reverse());
            }
            
         }
