7250aa322db075a1112d0cc18efdba5abe31b018
mailtosangeethadevidi-collab <mailtosangeethadevidi@gmail.com>
2026-01-30 16:28:44 +0530

    Time: 0 ms (100.00%) | Memory: 43.3 MB (86.08%) - LeetSync

---
+++ 75-sort-colors/sort-colors.java
@@ -0,0 +1,25 @@
+class Solution {
+    public void sortColors(int[] nums) {
+      int i=0;
+      int j=nums.length-1;
+      int k=0;
+      while(k<=j){
+        if(nums[k]==0){
+               int t=nums[k];
+               nums[k]=nums[i];
+               nums[i]=t;
+            i++;
+            k++;
+        }
+       else if(nums[k]==1){
+            k++;
+        }
+        else if(nums[k]==2){
+              int t=nums[k];
+              nums[k]=nums[j];
+              nums[j]=t;
+              j--;
+        }
+      }
+    }
+}
