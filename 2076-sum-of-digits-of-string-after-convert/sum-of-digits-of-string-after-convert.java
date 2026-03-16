fb06eecac48683786312a7cf6d597a0c0b890ae6
mailtosangeethadevidi-collab <mailtosangeethadevidi@gmail.com>
2026-03-16 16:30:35 +0530

    Time: 2 ms (58.30%) | Memory: 43 MB (85.02%) - LeetSync

---
+++ 2076-sum-of-digits-of-string-after-convert/sum-of-digits-of-string-after-convert.java
@@ -0,0 +1,26 @@
+class Solution {
+    public int getLucky(String s, int k) {
+       StringBuilder str=new StringBuilder();
+       for(int i=0;i<s.length();i++){
+        str.append(s.charAt(i)-'a'+1);
+       } 
+       int sum=0;
+       for(int i=0;i<str.length();i++){
+        sum+=(str.charAt(i)-'0');
+       }
+       
+      return sumfun(sum,--k);
+      
+    }
+    public static int sumfun(int n,int k ){
+        if(k==0)
+          return n;
+        int sum=0;
+        while(n>0){
+            sum+=(n%10);
+            n/=10;
+        }
+     return  sumfun(sum,--k); 
+    }
+        
+}
