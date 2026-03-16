4ed4e6466caf3d8bbc5bc7fd7f32754753f9b330
mailtosangeethadevidi-collab <mailtosangeethadevidi@gmail.com>
2026-03-16 12:16:12 +0530

    Time: 4 ms (99.61%) | Memory: 47.7 MB (82.92%) - LeetSync

--- 680-valid-palindrome-ii/valid-palindrome-ii.java
+++ 680-valid-palindrome-ii/valid-palindrome-ii.java
@@ -1,41 +1,27 @@
 class Solution {
     public boolean validPalindrome(String s) {
         int i=0,j=s.length()-1;
-        int count=0;
         while(i<j){
             char chi=s.charAt(i);
             char chj=s.charAt(j);
             if(chi!=chj){
-            count++;
-            StringBuilder sbi=new StringBuilder(s.substring(i+1,j+1));
-             boolean bi=isP(sbi);
-             StringBuilder sbj=new StringBuilder(s.substring(i,j));
-             boolean bj=isP(sbj);
-             if(bi||bj == true)
-               return true;
-             else 
-             return false;
+               return isP(s,i+1,j)||isP(s,i,j-1);
             }
-            if(count>1){
-             return false;
-             }
              i++;
              j--;
 
         }
        return true;
     }
-    public static boolean isP(StringBuilder one){
-        int i=0;
-        int j=one.length()-1;
-         while(i<j){
-            char chi=one.charAt(i);
-            char chj=one.charAt(j);
+    public static boolean isP(String one ,int left,int right){
+         while(left<right){
+            char chi=one.charAt(left);
+            char chj=one.charAt(right);
             if(chi!=chj){
               return false;
             }
-            i++;
-            j--;
+            left++;
+            right--;
          }
          return true;
     }
