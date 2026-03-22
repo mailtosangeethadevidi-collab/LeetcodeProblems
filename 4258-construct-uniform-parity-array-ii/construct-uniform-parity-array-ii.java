8b60121d45114d18847da29bd168512679b0adf4
mailtosangeethadevidi-collab <mailtosangeethadevidi@gmail.com>
2026-03-22 10:08:46 +0530

    Time: 5 ms (87.81%) | Memory: 121.6 MB (55.92%) - LeetSync

--- 4258-construct-uniform-parity-array-ii/construct-uniform-parity-array-ii.java
+++ 4258-construct-uniform-parity-array-ii/construct-uniform-parity-array-ii.java
@@ -1,14 +1,14 @@
 class Solution {
     public boolean uniformArray(int[] nums1) {
-    int n=nums1.length;
+    //int n=nums1.length;
     int min=Integer.MAX_VALUE;
     int even=0;
-        for(int i=0;i<n;i++){
+        for(int i=0;i<nums1.length;i++){
             min=Math.min(min,nums1[i]);
             if(nums1[i]%2==0)
               even++;
         }
-        if(min%2!=0 || even==n){
+        if(min%2!=0 || even==nums1.length){
             return true;
         }
         return false;
