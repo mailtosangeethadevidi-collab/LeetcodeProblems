01f605952d6149c9094afcf600b037e79a8d665c
mailtosangeethadevidi-collab <mailtosangeethadevidi@gmail.com>
2026-03-30 22:31:51 +0530

    Time: 3 ms (76.30%) | Memory: 47.8 MB (59.27%) - LeetSync

--- 1031-add-to-array-form-of-integer/add-to-array-form-of-integer.java
+++ 1031-add-to-array-form-of-integer/add-to-array-form-of-integer.java
@@ -2,9 +2,9 @@ class Solution {
     public List<Integer> addToArrayForm(int[] num, int k) {
      List<Integer> list = new ArrayList<>();
       int index=num.length-1;
-      int carry=0;
+      int carry=0,number=0;;
       while(k>0&&index>=0){
-       int number=num[index]+(k%10)+carry;
+        number=num[index]+(k%10)+carry;
        if(number>=10){
         list.add(number%10);
          carry= number/10;
@@ -17,7 +17,7 @@ class Solution {
        index--;
     }
      while(index>=0){
-        int number=num[index]+carry;
+         number=num[index]+carry;
         if(number>=10){
          list.add(number%10);
          carry= number/10;
@@ -29,7 +29,7 @@ class Solution {
         index--;
      }
       while(k>0){
-        int number=k%10+carry;
+         number=k%10+carry;
         if(number>=10){
          list.add(number%10);
          carry= number/10;
