01590add39387026a5f845abc7834caa44ce9a1f
mailtosangeethadevidi-collab <mailtosangeethadevidi@gmail.com>
2026-04-02 19:02:24 +0530

    Time: 7 ms (31.32%) | Memory: 48.3 MB (6.02%) - LeetSync

---
+++ 260-single-number-iii/single-number-iii.java
@@ -0,0 +1,20 @@
+class Solution {
+    public int[] singleNumber(int[] nums) {
+        Arrays.sort(nums);
+        int xor=0,k=0;
+        for(int i=0;i<nums.length-1;){
+            if(nums[i]==nums[i+1]){
+              i+=2;
+            }
+            else{
+                nums[k++]=nums[i];
+                i++;
+            }
+              
+        }
+        if(nums[nums.length-1]!=nums[nums.length-2]){
+            nums[1]=nums[nums.length-1];
+        }
+        return Arrays.copyOf(nums,2);
+    }
+}
