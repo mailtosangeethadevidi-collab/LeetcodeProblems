91e229657f34b035300e8fca10dfea173dbc8f93
mailtosangeethadevidi-collab <mailtosangeethadevidi@gmail.com>
2026-05-23 17:09:23 +0530

    Time: 1 ms (99.60%) | Memory: 46.9 MB (61.23%) - LeetSync

--- 1168-duplicate-zeros/duplicate-zeros.java
+++ 1168-duplicate-zeros/duplicate-zeros.java
@@ -1,17 +1,25 @@
 import java.util.ArrayList;
 class Solution {
     public void duplicateZeros(int[] arr) {
-       ArrayList<Integer> l = new ArrayList<Integer>(); 
-       for(int i=0;i<arr.length;i++){
-         if(arr[i]==0){
-            l.add(0);
-            l.add(0);
-         }
-         else
-         l.add(arr[i]);
-       }
-       for(int i=0;i<arr.length;i++){
-        arr[i]=l.get(i);
-       }
+        //lenght=arr lenght+num of zeroes
+     int newlenindex=arr.length-1;
+     for(int i=0;i<arr.length;i++){
+        if(arr[i]==0)
+          newlenindex++;
+     }
+     int arrlastindex=arr.length-1;
+     while(arrlastindex>=0){
+        if(newlenindex<arr.length){
+            arr[newlenindex]=arr[arrlastindex];
+        }
+        if(arr[arrlastindex]==0){
+            newlenindex--;
+            if(newlenindex<arr.length){
+                arr[newlenindex]=0;
+            }
+        }
+        newlenindex--;
+        arrlastindex--;
+     }
     }
 }
