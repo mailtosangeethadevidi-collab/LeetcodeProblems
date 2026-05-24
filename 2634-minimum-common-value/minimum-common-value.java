7c4503c46e55411ce427a1c3c159d49cd8816536
mailtosangeethadevidi-collab <mailtosangeethadevidi@gmail.com>
2026-05-24 14:46:13 +0530

    Time: 1 ms (97.38%) | Memory: 80.6 MB (33.71%) - LeetSync

--- 2634-minimum-common-value/minimum-common-value.java
+++ 2634-minimum-common-value/minimum-common-value.java
@@ -2,17 +2,15 @@ class Solution {
     public int getCommon(int[] nums1, int[] nums2) {
      int i=0;
      int j=0;
-    
+     
      while(i<nums1.length&&j<nums2.length){
-        while(i<nums1.length &&nums1[i]<nums2[j]){
+        if(nums1[i]<nums2[j])
           i++;
-        }
-         if(i<nums1.length&&j<nums2.length&&nums1[i]==nums2[j]){
-                return nums1[i];
-            }
-      while(i<nums1.length&&j<nums2.length &&nums2[j]<nums1[i]){
-          j++;
-        }
+        else if(nums2[j]<nums1[i])
+         j++;
+         else{
+           return nums1[i];
+         }
      }
      return -1;
     }
