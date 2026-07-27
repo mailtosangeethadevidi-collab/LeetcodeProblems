582eecdc806259df2dfad3c1c3b335e85d5db28b
mailtosangeethadevidi-collab <mailtosangeethadevidi@gmail.com>
2026-07-27 18:31:55 +0530

    Time: 1 ms (77.35%) | Memory: 42.3 MB (67.39%) - LeetSync

--- 202-happy-number/happy-number.java
+++ 202-happy-number/happy-number.java
@@ -1,24 +1,19 @@
 class Solution {
     public boolean isHappy(int n) {
-    int slow=digitSum(n);
-    int fast=digitSum(slow);
-    while(slow!=fast){
-        slow=digitSum(slow);
-        fast=digitSum(digitSum(fast));
-    }
-    if(slow==fast&&slow==1){
-        return true;
-    }
-    return false;
-
-    }
-    public int digitSum(int n){
+    while(n!=1&&n!=4){
         int sum=0;
         while(n>0){
             sum+=((n%10)*(n%10));
             n/=10;
         }
-        return sum;
+        if(sum==4)
+          return false;
+        else if(sum==1)
+         return true;
+
+       n=sum;
+    }
+    return n==1;
     }
     
 }
