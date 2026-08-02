9fd8e03590c9a219109aaf636afe684ea6e600a2
mailtosangeethadevidi-collab <mailtosangeethadevidi@gmail.com>
2026-08-02 11:32:04 +0530

    Time: 0 ms (100.00%) | Memory: 45.8 MB (98.47%) - LeetSync

--- 328-odd-even-linked-list/odd-even-linked-list.java
+++ 328-odd-even-linked-list/odd-even-linked-list.java
@@ -10,20 +10,22 @@
  */
 class Solution {
     public ListNode oddEvenList(ListNode head) {
-        if(head==null ||head.next==null){
+        if(head==null || head.next==null){
             return head;
         }
-        ListNode odd=head;
-        ListNode even=head.next;
-        ListNode evenhead=head.next;
-         while(even!=null&&even.next!=null){
-            odd.next=even.next;
-            odd=odd.next;
-            even.next=odd.next;
-            even=even.next;
-         }
-         odd.next=evenhead;
-        return head;
+    
+    ListNode odd=head;
+    ListNode even=head.next;
+    ListNode temp=even;
+    while(odd.next!=null&&even.next!=null){
+        odd.next=even.next;
+        odd=odd.next;
 
+        even.next=odd.next;
+        even=even.next;
+    }
+    odd.next=temp;
+    
+    return head;
         }
 }
