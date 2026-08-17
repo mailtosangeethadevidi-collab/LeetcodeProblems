6df6b5ce815eb168e10625d27e6af13858688dc4
mailtosangeethadevidi-collab <mailtosangeethadevidi@gmail.com>
2026-08-17 06:11:21 +0530

    Time: 3 ms (87.33%) | Memory: 65.4 MB (67.87%) - LeetSync

--- 1586-longest-subarray-of-1s-after-deleting-one-element/longest-subarray-of-1s-after-deleting-one-element.java
+++ 1586-longest-subarray-of-1s-after-deleting-one-element/longest-subarray-of-1s-after-deleting-one-element.java
@@ -1,41 +1,35 @@
 class Solution {
     public int longestSubarray(int[] nums) {
-        int n=nums.length;
-        if(n==1){
-            return 0;
+       int l=0;
+       int r=0;
+       int maxOnes=0;
+       int del=0;
+        int onesCount=0;
+       int len=nums.length;
+       int flag=0;
+       while(r<len){
+        if(nums[r]==0){
+            flag=1;
+            del++;
         }
-
-        int flag=0;
-        int l=0;
-        int r=0;
-        int del=0;
-        int count=0;
-        int maxcount=0;
-        while(r<n){
-            if(nums[r]==0){
-                flag=1;
-                del++;
-            }
-            if(nums[r]==1){
-                count++;
-            }
-            while(l<n&&del>1){
-               if(nums[l]==0){
-                 del--;
-               }
-               else{
-                count--;
-               }
-              l++;
-            }
-            if(del<=1){
-              maxcount=Math.max(count,maxcount);
-            }
-            r++;
+        if(nums[r]==1){
+            onesCount++;
         }
-        if(flag==0){
-            return n-1;
+        while(del>1){
+            if(nums[l]==0){
+                del--;
+            }
+            if(nums[l]==1){
+                onesCount--;
+            }
+            l++;
         }
-        return maxcount;
+        maxOnes=Math.max(maxOnes,onesCount);
+       r++;
+       }
+       if(flag==0){
+        return len-1;
+       }
+       return maxOnes;
     }
 }
