7f7f3982c3ef79352467645d372f4ae8daa85634
mailtosangeethadevidi-collab <mailtosangeethadevidi@gmail.com>
2026-09-04 10:29:28 +0530

    Time: 1 ms (99.12%) | Memory: 46.9 MB (70.10%) - LeetSync

--- 3215-matrix-similarity-after-cyclic-shifts/matrix-similarity-after-cyclic-shifts.java
+++ 3215-matrix-similarity-after-cyclic-shifts/matrix-similarity-after-cyclic-shifts.java
@@ -1,36 +1,14 @@
 class Solution {
     public boolean areSimilar(int[][] mat, int k) {
-        int t[][] = new int[mat.length][];
-        for (int i = 0; i < mat.length; i++) {
-            t[i] = mat[i].clone();
-        }
-        while (k-- != 0) {
-            int i = 0;
-            boolean even = true;
-            while (i < mat.length) {
-                if (even) {
-                    int temp = mat[i][0];
-                    int j = 0;
-                    for (j = 0; j < mat[0].length - 1; j++) {
-                        mat[i][j] = mat[i][j + 1];
-                    }
-                    mat[i][j] = temp;
-
-                    even = false;
-                } else {
-                    int temp = mat[i][mat[0].length - 1];
-                    int j = 0;
-                    for (j = mat[0].length - 1; j >= 1; j--) {
-                        mat[i][j] = mat[i][j - 1];
-                    }
-                    mat[i][j] = temp;
-                    even = true;
+        int shift=k%mat[0].length;
+        for(int row[]:mat){
+            for(int i=0;i<mat[0].length;i++){
+                if(row[i]!=row[(i+shift)%mat[0].length]){
+                    return false;
                 }
-                i++;
-
             }
         }
-       
-        return Arrays.deepEquals(t, mat);
+     return true;
     }
+
 }
