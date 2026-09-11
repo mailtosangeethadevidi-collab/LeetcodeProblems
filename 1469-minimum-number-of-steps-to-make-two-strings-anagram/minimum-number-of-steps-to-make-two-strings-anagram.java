a60da3c6f0f73c98715be2784e27abc5eef72a09
mailtosangeethadevidi-collab <mailtosangeethadevidi@gmail.com>
2026-09-11 18:43:03 +0530

    Time: 9 ms (48.37%) | Memory: 47.8 MB (11.69%) - LeetSync

--- 1469-minimum-number-of-steps-to-make-two-strings-anagram/minimum-number-of-steps-to-make-two-strings-anagram.java
+++ 1469-minimum-number-of-steps-to-make-two-strings-anagram/minimum-number-of-steps-to-make-two-strings-anagram.java
@@ -1,25 +1,19 @@
 class Solution {
     public int minSteps(String s, String t) {
-        int srr[]=new int[26];
-        int trr[]=new int[26];
+       
+            
+        int freq[]=new int[26];
         int len=s.length();
         for(int i=0;i<len;i++){
-            srr[s.charAt(i)-'a']++;
-            trr[t.charAt(i)-'a']++;
+            freq[s.charAt(i)-'a']++;
+            freq[t.charAt(i)-'a']--;
+
         }
         int count=0;
-        for(int i=0;i<len;i++){
-            char ch =t.charAt(i);
-            if(trr[ch-'a']!=-1){
-            if(srr[ch-'a']==0){
-                count+=trr[ch-'a'];
+        for(int i=0;i<26;i++){
+            if(freq[i]>0){
+                  count+=freq[i];
             }
-            else if(trr[ch-'a']>srr[ch-'a']){
-                count+=(trr[ch-'a']-srr[ch-'a']);
-            }
-            }
-            trr[ch-'a']=-1;
-            
         }
         return count;
 
