4c87493feec0941c5995d89eedb19e0f63730844
mailtosangeethadevidi-collab <mailtosangeethadevidi@gmail.com>
2026-09-21 18:38:01 +0530

    Time: 3 ms (85.22%) | Memory: 48.1 MB (64.52%) - LeetSync

--- 240-search-a-2d-matrix-ii/search-a-2d-matrix-ii.java
+++ 240-search-a-2d-matrix-ii/search-a-2d-matrix-ii.java
@@ -1,26 +1,25 @@
 class Solution {
     public boolean searchMatrix(int[][] matrix, int target) {
         
-        for(int row=0;row<matrix.length;row++){
-            int low=0;
-            int high=matrix[0].length-1;
-            if(matrix[row][0]<=target && target <=matrix[row][high]){
-                while(low<=high){
-                    int mid=(low+high)/2;
-
-                    if(matrix[row][mid]==target){
+       
+            int row=0;
+            int col=matrix[0].length-1;
+           
+                while(row<matrix.length&&col>=0){
+                   
+                    if(matrix[row][col]==target){
                         return true;
                     }
-                    else if(matrix[row][mid]>target){
-                        high=mid-1;
+                    else if(matrix[row][col]>target){
+                       col--;
                     }
                     else{
-                        low=mid+1;
+                        row++;
                     }
                 }
-            }
+            
            
-        }
+        
         return false;
        
     }
